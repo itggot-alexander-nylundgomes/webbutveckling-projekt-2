abdf98e0c9de143e24e1c3459167f6a28cdc8e79
itggot-alexander-nylundgomes <43437952+itggot-alexander-nylundgomes@users.noreply.github.com>
2018-12-20 13:11:11 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,3 +2,13 @@ function doStuff(event){
     let fullmeny = document.querySelector(".fullmeny")
  fullmeny.classList.toggle("activate")
  }
+
+ function doStuff_clothes(event){
+    let nav = document.querySelector("nav")
+    nav.classList.toggle("activate_clothes_menu")
+    
+    let clothestxt = document.querySelector(".clothestxt")
+    clothestxt.classList.toggle("activate_clothes_text")
+ }
+
+ 
