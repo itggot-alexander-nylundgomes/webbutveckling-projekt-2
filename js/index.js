6823c68f398a864a2e1a3af3c1df8475653a2a08
itggot-alexander-nylundgomes <43437952+itggot-alexander-nylundgomes@users.noreply.github.com>
2019-01-10 15:45:29 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -9,6 +9,17 @@ function doStuff(event){
     
     let clothestxt = document.querySelector(".clothestxt")
     clothestxt.classList.toggle("activate_clothes_text")
+
+    let lowertxt = document.querySelector(".lowertxt")
+    lowertxt.classList.toggle("activate_lowertxt")
+
+    let filter_pop_box = document.querySelector(".filter_pop_box")
+    filter_pop_box.classList.toggle("activate_filter_pop_box")
+ }
+
+ function prisbox(event){
+    let activate_filter_pop_box = document.querySelector("activate_filter_pop_box")
+    activate_filter_pop_box.classList.toggle(".activate_prisbox")
  }
 
  
